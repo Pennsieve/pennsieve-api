a7ec279b43c39dd54c74d6ba6739d5cded0c9cb7
Anand Mallik <sankho.mallik@gmail.com>
2021-03-23 21:32:51 -0400

    comments out claim approaches, starts over with basic validation + assembling the cognito public keys lib

--- core/src/main/scala/com/blackfynn/aws/cognito/CognitoJWTAuthenticator.scala
+++ core/src/main/scala/com/blackfynn/aws/cognito/CognitoJWTAuthenticator.scala
@@ -1,21 +1,26 @@
 package com.blackfynn.aws.cognito
 
 import com.blackfynn.auth.middleware.Jwt
-import io.circe.{Decoder, Encoder, HCursor, Parser}
+import io.circe.{Decoder, Encoder, HCursor, Json, Parser, ParsingFailure}
 import io.circe.derivation.deriveDecoder
 import io.circe.generic.extras.semiauto.deriveEncoder
-import pdi.jwt.{JwtAlgorithm, JwtCirce}
+import pdi.jwt.{JwtAlgorithm, JwtCirce, JwtClaim}
 
-sealed trait CognitoClaimType {}
+import scala.io.Source.fromURL
+
+/*
+trait CognitoClaimType {}
 
 object CognitoClaimType {
-  implicit def claimTypeEncoder: Encoder[CognitoClaimType] = deriveEncoder[CognitoClaimType]
-  implicit def claimTypeDecoder: Decoder[CognitoClaimType] = deriveDecoder[CognitoClaimType]
+  implicit def claimTypeEncoder: Encoder[CognitoClaimType] =
+    deriveEncoder[CognitoClaimType]
+  implicit def claimTypeDecoder: Decoder[CognitoClaimType] =
+    deriveDecoder[CognitoClaimType]
 }
 
-case class CognitoClaim(
-  cognitoUsername: String
-) extends CognitoClaimType
+case class CognitoClaim(cognitoUsername: String) extends CognitoClaimType {
+  def toJwtClaim: JwtClaim = JwtClaim(content = cognitoUsername)
+}
 
 object CognitoClaim {
   private def v2Decoder: Decoder[CognitoClaim] = deriveDecoder[CognitoClaim]
@@ -24,29 +29,58 @@ object CognitoClaim {
     final def apply(c: HCursor): Decoder.Result[CognitoClaim] =
       for {
         cognitoUsername <- c.downField("cognito:username").as[String]
-      } yield
-        CognitoClaim(
-          cognitoUsername = cognitoUsername
-        )
+      } yield CognitoClaim(cognitoUsername = cognitoUsername)
   }
 
-  implicit def cognitoClaimEncoder: Encoder[CognitoClaim] = deriveEncoder[CognitoClaim]
-  implicit def cognitoClaimDecoder: Decoder[CognitoClaim] = List(v2Decoder, v1Decoder).reduceLeft(_ or _)
+  implicit def cognitoClaimEncoder: Encoder[CognitoClaim] =
+    deriveEncoder[CognitoClaim]
+  implicit def cognitoClaimDecoder: Decoder[CognitoClaim] =
+    List(v2Decoder, v1Decoder).reduceLeft(_ or _)
 }
+ */
 
 object CognitoJWTAuthenticator extends Parser {
 
-  def validateToken(token: Jwt.Token): Boolean = {
-    false
+  var keysJson: String = ""
+
+  def getKeysUrl(awsRegion: String, userPoolId: String): String = {
+    s"https://cognito-idp.$awsRegion.amazonaws.com/$userPoolId/.well-known/jwks.json"
   }
 
+  def getKeysJson(awsRegion: String, userPoolId: String): String = {
+    if (keysJson != "") {
+      return keysJson
+    }
+
+    keysJson = fromURL(getKeysUrl(awsRegion, userPoolId)).mkString
+
+    return keysJson
+  }
+
+  def validateToken(token: String): Either[Throwable, Boolean] = {
+    for {
+      claims <- JwtCirce
+        .decode(token)
+        .toEither
+    } yield {
+      true
+    }
+  }
+
+  /*
   def getUserIdFromToken(token: Jwt.Token): Either[Throwable, String] = {
     for {
       claim <- JwtCirce
         .decode(token.value, "jwt-key", Seq(JwtAlgorithm.HS256))
         .toEither
       content <- decode[CognitoClaim](claim.content)
-    } yield
+    } yield {
       content.cognitoUsername
+
+      // TODO: now take the above and get Blackfynn ID from DB
+    }
   }
+   */
+
+  override def parse(input: String): Either[ParsingFailure, Json] = ???
 }
--- core/src/test/scala/com/blackfynn/aws/cognito/CognitoJWTAuthenticatorSpec.scala
+++ core/src/test/scala/com/blackfynn/aws/cognito/CognitoJWTAuthenticatorSpec.scala
@@ -1,21 +1,37 @@
 package com.blackfynn.aws.cognito
 
-import org.scalatest.{ FlatSpec, Matchers }
+import com.blackfynn.utilities.Container
+import org.scalatest.{ BeforeAndAfter, FlatSpec, Matchers }
+import pdi.jwt.{ JwtAlgorithm, JwtCirce, JwtClaim }
 
-class CognitoJWTAuthenticatorSpec extends FlatSpec with Matchers {
+class CognitoJWTAuthenticatorSpec
+    extends FlatSpec
+    with Matchers
+    with BeforeAndAfter {
+
+  var testToken: String =
+    JwtCirce.encode(JwtClaim("claim"))
+  var invalidToken: String =
+    JwtCirce.encode(JwtClaim("claim"))
+
+  // before {}
+
+  "getKeysUrl" should "return the correct URL given arguments" in {
+    CognitoJWTAuthenticator.getKeysUrl("foo", "bar") should equal(
+      "https://cognito-idp.foo.amazonaws.com/bar/.well-known/jwks.json"
+    )
+  }
 
   "validateToken" should "return true if supplied token is valid" in {
-    CognitoJWTAuthenticator.validateToken("valid_token") should equal(true)
+    CognitoJWTAuthenticator.validateToken(testToken) should equal(true)
   }
 
   "validateToken" should "return false if supplied token is invalid" in {
-    CognitoJWTAuthenticator.validateToken("invalid_token") should equal(false)
+    CognitoJWTAuthenticator.validateToken(invalidToken) should equal(false)
   }
 
-  "getUserIdFromToken" should "return user ID from supplied token payload" in {
-    CognitoJWTAuthenticator.getUserIdFromToken(token = "valid_token") should equal(
-      "userid"
-    )
-  }
+//  "getUserIdFromToken" should "return user ID from supplied token payload" in {
+//    CognitoJWTAuthenticator.getUserIdFromToken(testToken) should equal("userid")
+//  }
 
 }
