b04bba75b51da2f98e4ca2958e7a55ab4fa748eb
Bo Marchman <bo.marchman@gmail.com>
2021-04-01 15:53:33 -0400

    Token cognito IDs must be unique

--- core/src/test/scala/com/blackfynn/test/PostgresDockerContainer.scala
+++ core/src/test/scala/com/blackfynn/test/PostgresDockerContainer.scala
@@ -24,7 +24,7 @@ import java.time.Duration
 
 final class PostgresDockerContainerImpl
     extends PostgresContainerImpl(
-      dockerImage = "pennsieve/pennsievedb:V20210401134920"
+      dockerImage = "pennsieve/pennsievedb:V20210401155037"
     )
 
 trait PostgresDockerContainer extends StackedDockerContainer {
@@ -35,7 +35,7 @@ trait PostgresDockerContainer extends StackedDockerContainer {
 
 final class PostgresSeedDockerContainerImpl
     extends PostgresContainerImpl(
-      dockerImage = "pennsieve/pennsievedb:V20210401134920-seed"
+      dockerImage = "pennsieve/pennsievedb:V20210401155037-seed"
     )
 
 trait PostgresSeedDockerContainer extends StackedDockerContainer {
