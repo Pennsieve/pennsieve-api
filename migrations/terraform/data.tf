144af9d2e7c20996eab6b199f74adfcb14d7b2a2
JeremyLecoeur <jeremy@blackfynn.com>
2021-04-02 10:54:19 -0400

    data postgres

--- migrations/terraform/data.tf
+++ migrations/terraform/data.tf
@@ -10,3 +10,13 @@ data "terraform_remote_state" "pennsieve_postgres" {
     region = "us-east-1"
   }
 }
+
+data "terraform_remote_state" "data" {
+  backend = "s3"
+
+  config = {
+    bucket = "${var.aws_account}-terraform-state"
+    key    = "aws/${data.aws_region.current_region.name}/${var.vpc_name}/${var.environment_name}/data-postgres/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
