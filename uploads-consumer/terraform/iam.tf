ba437f1ed890ebcef223cacb9a80ab3622fe5589
Bo Marchman <bo.marchman@gmail.com>
2021-04-18 19:11:30 -0400

    Add IAM permissions for SPARC storage buckets

--- uploads-consumer/terraform/iam.tf
+++ uploads-consumer/terraform/iam.tf
@@ -132,8 +132,8 @@ data "aws_iam_policy_document" "ecs_task_iam_policy_document" {
       "${data.terraform_remote_state.platform_infrastructure.outputs.uploads_bucket_arn}/*",
       data.terraform_remote_state.platform_infrastructure.outputs.storage_bucket_arn,
       "${data.terraform_remote_state.platform_infrastructure.outputs.storage_bucket_arn}/*",
-      # data.terraform_remote_state.platform_infrastructure.outputs.sparc_storage_bucket_arn,
-      # "${data.terraform_remote_state.platform_infrastructure.outputs.sparc_storage_bucket_arn}/*",
+      data.terraform_remote_state.platform_infrastructure.outputs.sparc_storage_bucket_arn,
+      "${data.terraform_remote_state.platform_infrastructure.outputs.sparc_storage_bucket_arn}/*",
     ]
   }
 }
