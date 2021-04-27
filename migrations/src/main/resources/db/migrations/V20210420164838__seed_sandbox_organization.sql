5985c2dd8edc06ebaedd58924154b62ab66fdfb9
Anand Mallik <sankho.mallik@gmail.com>
2021-04-27 12:17:45 -0400

    adjusts migration to ensure ID is from the last attached org

--- migrations/src/main/resources/db/migrations/V20210420164838__seed_sandbox_organization.sql
+++ migrations/src/main/resources/db/migrations/V20210420164838__seed_sandbox_organization.sql
@@ -5,8 +5,5 @@ INSERT INTO organizations (name, slug, node_id, encryption_key_id) VALUES (
     'NO_ENCRYPTION_KEY'  -- encryption_key_id: String
 );
 
-INSERT INTO feature_flags (organization_id, feature, enabled) VALUES (
-    currval(pg_get_serial_sequence('organizations', 'id')),
-    'sandbox_org_feature',
-    true
-)
+INSERT INTO feature_flags (organization_id, feature, enabled)
+SELECT (id), 'sandbox_org_feature', true FROM organizations WHERE node_id = 'N:organization:88c078d6-1827-4e14-867b-801448fe0622'
