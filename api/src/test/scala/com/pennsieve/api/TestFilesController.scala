c1605abb1ecb73939bdc3993b6f9f396c7d3bb81
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2021-10-01 09:16:33 -0400

    updated tests

--- api/src/test/scala/com/pennsieve/api/TestFilesController.scala
+++ api/src/test/scala/com/pennsieve/api/TestFilesController.scala
@@ -902,6 +902,7 @@ class TestFilesController extends FlatSpec with DataSetTestMixin with ApiSuite {
             (parsedBody \ "manifest" \ "content").extract[List[Upload]].head
           val pkg = packageManager.get(job.packageId).await.right.value
           val srcFiles = fileManager.getSources(pkg).await.right.value
+//          assert(srcFiles == 1)
           assert(
             srcFiles.map(_.name).toSet == Set("testIMG.img", "testIMG.hdr")
           )
