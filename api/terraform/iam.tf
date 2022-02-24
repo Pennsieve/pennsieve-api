c3130f30e79919d7449215b71daa4f9809c41443
Michael Uftring <michael.uftring@gmail.com>
2022-02-24 14:15:39 -0500

    add cognito-idp:AdminDisableProviderForUser to access policy

--- api/terraform/iam.tf
+++ api/terraform/iam.tf
@@ -122,6 +122,7 @@ data "aws_iam_policy_document" "cognito_iam_policy_document" {
 
     actions = [
       "cognito-idp:AdminCreateUser",
+      "cognito-idp:AdminDisableProviderForUser",
     ]
 
     resources = [
