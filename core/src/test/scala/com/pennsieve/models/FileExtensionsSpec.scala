225be72a1a2306a0e9c89ecda6c3d36ffa5138a4
Michael Uftring <michael.uftring@gmail.com>
2022-07-12 12:40:17 -0400

    altered 'utilities' import to assist in resolving package object fix (slightly misplaced test source file)

--- core/src/test/scala/com/pennsieve/models/FileExtensionsSpec.scala
+++ core/src/test/scala/com/pennsieve/models/FileExtensionsSpec.scala
@@ -16,19 +16,21 @@
 
 package com.pennsieve.models
 
-import com.pennsieve.core.utilities
+import com.pennsieve.core.{ utilities => coreUtilities }
 import org.scalatest.{ FlatSpec, Matchers }
 
 class FileExtensionsSpec extends FlatSpec with Matchers {
 
   "splitFileName" should "return correctly for normal file names" in {
 
-    utilities.splitFileName("picture.png") should equal(("picture", ".png"))
-    utilities.splitFileName("matlab.m") should equal(("matlab", ".m"))
-    utilities.splitFileName("msaccess.mat") should equal(("msaccess", ".mat"))
-    utilities.splitFileName("nicolet.e") should equal(("nicolet", ".e"))
-    utilities.splitFileName("nifti.nii") should equal(("nifti", ".nii"))
-    utilities.splitFileName("annotation.bfannot") should equal(
+    coreUtilities.splitFileName("picture.png") should equal(("picture", ".png"))
+    coreUtilities.splitFileName("matlab.m") should equal(("matlab", ".m"))
+    coreUtilities.splitFileName("msaccess.mat") should equal(
+      ("msaccess", ".mat")
+    )
+    coreUtilities.splitFileName("nicolet.e") should equal(("nicolet", ".e"))
+    coreUtilities.splitFileName("nifti.nii") should equal(("nifti", ".nii"))
+    coreUtilities.splitFileName("annotation.bfannot") should equal(
       ("annotation", ".bfannot")
     )
 
@@ -36,17 +38,19 @@ class FileExtensionsSpec extends FlatSpec with Matchers {
 
   "splitFileName" should "return correctly for file names with complex extensions" in {
 
-    utilities.splitFileName("reading.moberg.gz") should equal(
+    coreUtilities.splitFileName("reading.moberg.gz") should equal(
       ("reading", ".moberg.gz")
     )
-    utilities.splitFileName("ts.mefd.gz") should equal(("ts", ".mefd.gz"))
-    utilities.splitFileName("nifti.nii.gz") should equal(("nifti", ".nii.gz"))
+    coreUtilities.splitFileName("ts.mefd.gz") should equal(("ts", ".mefd.gz"))
+    coreUtilities.splitFileName("nifti.nii.gz") should equal(
+      ("nifti", ".nii.gz")
+    )
 
   }
 
   "splitFileName" should "return the longest matching extension" in {
 
-    utilities.splitFileName("image.ome.tiff") should equal(
+    coreUtilities.splitFileName("image.ome.tiff") should equal(
       ("image", ".ome.tiff")
     )
 
@@ -54,23 +58,23 @@ class FileExtensionsSpec extends FlatSpec with Matchers {
 
   "splitFileName" should "return an empty string extension when it doesn't match a supported extension" in {
 
-    utilities.splitFileName("bad.blah") should equal(("bad.blah", ""))
-    utilities.splitFileName("bad.blah.gz") should equal(("bad.blah.gz", ""))
+    coreUtilities.splitFileName("bad.blah") should equal(("bad.blah", ""))
+    coreUtilities.splitFileName("bad.blah.gz") should equal(("bad.blah.gz", ""))
 
   }
 
   "splitFileName" should "handles file names that include S3 directory paths appropriately" in {
 
-    utilities.splitFileName(
+    coreUtilities.splitFileName(
       "test@pennsieve.org/545d1f2a-1b0b-4888-91fe-af221e1d55aa/simple.csv"
     ) should equal(("simple", ".csv"))
-    utilities.splitFileName(
+    coreUtilities.splitFileName(
       "test@pennsieve.org/545d1f2a-1b0b-4888-91fe-af221e1d55aa/reading.moberg.gz"
     ) should equal(("reading", ".moberg.gz"))
-    utilities.splitFileName(
+    coreUtilities.splitFileName(
       "test@pennsieve.org/545d1f2a-1b0b-4888-91fe-af221e1d55aa/nifti.nii.gz"
     ) should equal(("nifti", ".nii.gz"))
-    utilities.splitFileName(
+    coreUtilities.splitFileName(
       "test@pennsieve.org/545d1f2a-1b0b-4888-91fe-af221e1d55aa/bad.blah"
     ) should equal(("bad.blah", ""))
 
