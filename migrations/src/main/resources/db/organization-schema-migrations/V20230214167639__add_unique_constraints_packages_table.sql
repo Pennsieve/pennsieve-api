863d4bee2aea987140c085de64124b1e6a18a75a
ddefreitas <darrell.defreitas@gmail.com>
2023-02-14 13:33:59 -0500

    Remove individual org

--- migrations/src/main/resources/db/organization-schema-migrations/V20230214167639__add_unique_constraints_packages_table.sql
+++ migrations/src/main/resources/db/organization-schema-migrations/V20230214167639__add_unique_constraints_packages_table.sql
@@ -1,3 +1,3 @@
-CREATE UNIQUE INDEX packages_name_dataset_id__parent_id_null ON "19".packages ("name", "dataset_id") WHERE parent_id IS NULL;
+CREATE UNIQUE INDEX packages_name_dataset_id__parent_id_null ON packages ("name", "dataset_id") WHERE parent_id IS NULL;
 
-CREATE UNIQUE INDEX packages_name_dataset_id ON "19".packages ("name", "parent_id")
+CREATE UNIQUE INDEX packages_name_dataset_id ON packages ("name", "parent_id")
