3b5c612ba54a5cf5b54c5bf4a7887a9806b1c23c
ddefreitas <darrell.defreitas@gmail.com>
2023-02-14 15:42:23 -0500

    Update V20230214167639__add_unique_constraints_packages_table.sql
    Remove index on name and parent id

--- migrations/src/main/resources/db/organization-schema-migrations/V20230214167639__add_unique_constraints_packages_table.sql
+++ migrations/src/main/resources/db/organization-schema-migrations/V20230214167639__add_unique_constraints_packages_table.sql
@@ -1,3 +1,1 @@
 CREATE UNIQUE INDEX packages_name_dataset_id__parent_id_null ON packages ("name", "dataset_id") WHERE parent_id IS NULL;
-
-CREATE UNIQUE INDEX packages_name_dataset_id ON packages ("name", "parent_id")
