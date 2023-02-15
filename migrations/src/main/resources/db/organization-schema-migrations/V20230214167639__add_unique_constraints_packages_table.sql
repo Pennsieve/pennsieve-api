6382aec0b044b8da343d3a0fb01e46d6c66455ae
ddefreitas <darrell.defreitas@gmail.com>
2023-02-15 16:28:40 -0500

    Update unique indicies
    With help from @jwagenaar  and @jfromme

--- migrations/src/main/resources/db/organization-schema-migrations/V20230214167639__add_unique_constraints_packages_table.sql
+++ migrations/src/main/resources/db/organization-schema-migrations/V20230214167639__add_unique_constraints_packages_table.sql
@@ -1,1 +1,3 @@
-CREATE UNIQUE INDEX packages_name_dataset_id__parent_id_null ON packages ("name", "dataset_id") WHERE parent_id IS NULL;
+create unique index packages_name_dataset_id__parent_id_null_idx on packages (name,dataset_id) where parent_id is null
+
+create unique index packages_name_dataset_id_parent_id__parent_id_not_null_idx on packages (name,dataset_id,parent_id) where parent_id is not null
