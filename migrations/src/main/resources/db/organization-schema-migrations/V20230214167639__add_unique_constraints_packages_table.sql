76db99c3e42703719353040bffaf214c78a3c0be
ddefreitas <darrell.defreitas@gmail.com>
2023-02-15 16:34:45 -0500

    Add semicolons for multi line statement

--- migrations/src/main/resources/db/organization-schema-migrations/V20230214167639__add_unique_constraints_packages_table.sql
+++ migrations/src/main/resources/db/organization-schema-migrations/V20230214167639__add_unique_constraints_packages_table.sql
@@ -1,3 +1,3 @@
-create unique index packages_name_dataset_id__parent_id_null_idx on packages (name,dataset_id) where parent_id is null
+create unique index packages_name_dataset_id__parent_id_null_idx on packages (name,dataset_id) where parent_id is null;
 
-create unique index packages_name_dataset_id_parent_id__parent_id_not_null_idx on packages (name,dataset_id,parent_id) where parent_id is not null
+create unique index packages_name_dataset_id_parent_id__parent_id_not_null_idx on packages (name,dataset_id,parent_id) where parent_id is not null;
