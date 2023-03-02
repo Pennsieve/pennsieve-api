4670bb30fb26cb34dc1f9d3fdd037558483a7536
ddefreitas <darrell.defreitas@gmail.com>
2023-03-01 19:13:46 -0500

    Update migration to be in anonymous block

--- migrations/src/main/resources/db/organization-schema-migrations/V20230214167639__add_unique_constraints_packages_table.sql
+++ migrations/src/main/resources/db/organization-schema-migrations/V20230214167639__add_unique_constraints_packages_table.sql
@@ -1,3 +1,11 @@
-create unique index packages_name_dataset_id__parent_id_null_idx on packages (name,dataset_id,"type") where parent_id is null;
+do $$
+begin
 
-create unique index packages_name_dataset_id_parent_id__parent_id_not_null_idx on packages (name,dataset_id,"type",parent_id) where parent_id is NOT null;
+    create unique index packages_name_dataset_id__parent_id_null_idx on packages (name,dataset_id,"type") where parent_id is null;
+
+    create unique index packages_name_dataset_id_parent_id__parent_id_not_null_idx on packages (name,dataset_id,"type",parent_id) where parent_id is NOT null;
+
+exception when others then
+	rollback;
+
+end; $$
