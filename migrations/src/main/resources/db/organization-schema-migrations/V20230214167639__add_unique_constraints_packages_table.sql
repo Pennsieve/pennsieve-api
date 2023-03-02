e21d8dad4ab611b245db1136d4adfc6601b376ae
ddefreitas <darrell.defreitas@gmail.com>
2023-03-02 11:38:39 -0500

    Add exception output

--- migrations/src/main/resources/db/organization-schema-migrations/V20230214167639__add_unique_constraints_packages_table.sql
+++ migrations/src/main/resources/db/organization-schema-migrations/V20230214167639__add_unique_constraints_packages_table.sql
@@ -1,11 +1,14 @@
 do $$
+declare schema_name text;
 begin
 
-    create unique index packages_name_dataset_id__parent_id_null_idx on packages (name,dataset_id,"type") where parent_id is null;
-
-    create unique index packages_name_dataset_id_parent_id__parent_id_not_null_idx on packages (name,dataset_id,"type",parent_id) where parent_id is NOT null;
-
+	select current_schema() into schema_name;
+	create unique index packages_name_dataset_id__parent_id_null_idx on packages (name,dataset_id,"type") where parent_id is null;
+	create unique index packages_name_dataset_id_parent_id__parent_id_not_null_idx on packages (name,dataset_id,"type",parent_id) where parent_id is NOT null;
+	
 exception when others then
+	raise notice 'Error with schema: %', schema_name;
+	raise notice '% %', SQLERRM, SQLSTATE;
 	rollback;
 
 end; $$
