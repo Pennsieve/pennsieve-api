5810b1099d1b1b11f5b3cfbee6523eb9d2443d51
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2023-05-31 15:02:34 -0400

    update column name

--- migrations/src/main/resources/db/organization-schema-migrations/V20230531145242__update_webhook_info.sql
+++ migrations/src/main/resources/db/organization-schema-migrations/V20230531145242__update_webhook_info.sql
@@ -1,2 +1,2 @@
 ALTER TABLE webhooks ADD COLUMN target_info JSONB default '[]';
-ALTER TABLE webhooks ADD COLUMN message_template JSONB default '[]';
+ALTER TABLE webhooks ADD COLUMN message_schema JSONB default '[]';
