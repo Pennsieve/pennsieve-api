9223261bee245f809f037ab54d8f852c250f3eb9
ddefreitas <darrell.defreitas@gmail.com>
2023-07-05 16:56:56 -0400

    Add function signature

--- core/src/main/scala/com/pennsieve/aws/s3/S3.scala
+++ core/src/main/scala/com/pennsieve/aws/s3/S3.scala
@@ -87,6 +87,8 @@ trait S3Trait {
   def deleteObject(o: S3Object): Either[Throwable, Unit] =
     deleteObject(o.getBucketName, o.getKey)
 
+  def deleteAllVersions(bucket: String, key: String, client: AmazonS3): Unit
+
   def deleteObjectsByKeys(
     bucket: String,
     keys: Seq[String],
@@ -306,6 +308,7 @@ class S3(val client: AmazonS3) extends S3Trait {
       }
     }
 
+  // Gathers up all versions of a $key and does a hard delete
   def deleteAllVersions(bucket: String, key: String, client: AmazonS3): Unit = {
     val versionListing: VersionListing = client.listVersions(bucket, key)
     val deleteRequests = versionListing.getVersionSummaries.asScala.map { versionSummary =>
