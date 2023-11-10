26c28a167ca42a6337dffe5acb11c9845b163a66
Michael Uftring <michael.uftring@gmail.com>
2023-11-10 11:56:36 -0500

    add DatasetReference model class

---
+++ core-models/src/main/scala/com/pennsieve/models/DatasetReference.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2021 University of Pennsylvania
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pennsieve.models
+
+import io.circe.generic.semiauto.{ deriveDecoder, deriveEncoder }
+import io.circe.{ Decoder, Encoder }
+
+import java.time.ZonedDateTime
+
+case class NameValueProperty(name: String, value: String)
+object NameValueProperty {
+  implicit val encoder: Encoder[NameValueProperty] =
+    deriveEncoder[NameValueProperty]
+  implicit val decoder: Decoder[NameValueProperty] =
+    deriveDecoder[NameValueProperty]
+}
+
+case class DatasetReference(
+  id: Int,
+  userId: Int,
+  referenceOrder: Int,
+  referenceType: String,
+  referenceId: String,
+  properties: Option[List[NameValueProperty]],
+  tags: Option[List[String]],
+  createdAt: ZonedDateTime = ZonedDateTime.now,
+  updatedAt: ZonedDateTime = ZonedDateTime.now
+)
+
+object DatasetReference {
+  implicit val encoder: Encoder[DatasetReference] =
+    deriveEncoder[DatasetReference]
+  implicit val decoder: Decoder[DatasetReference] =
+    deriveDecoder[DatasetReference]
+
+  val tupled = (this.apply _).tupled
+}
