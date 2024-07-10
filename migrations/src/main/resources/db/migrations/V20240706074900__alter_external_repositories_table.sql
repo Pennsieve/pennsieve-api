e96f9084051cbbe3d07718b3475897ceb5410e8b
Michael Uftring <michael.uftring@gmail.com>
2024-07-10 15:04:45 -0400

    revert V20240706074900 migration back to previous commit

--- migrations/src/main/resources/db/migrations/V20240706074900__alter_external_repositories_table.sql
+++ migrations/src/main/resources/db/migrations/V20240706074900__alter_external_repositories_table.sql
@@ -1,16 +1,2 @@
-/*
-** rename 'repository' to 'origin' to reduce confusion
-*/
 ALTER TABLE external_repositories RENAME COLUMN repository TO origin;
-
-/*
-** add 'status' column to track status of the external repo: enabled, disabled, suspended
-*/
 ALTER TABLE external_repositories ADD COLUMN status TEXT NOT NULL;
-
-/*
-** drop constraints: permit 'label', 'marker', and 'release_date' to be NULL
-*/
-ALTER TABLE external_repositories ALTER COLUMN label DROP NOT NULL;
-ALTER TABLE external_repositories ALTER COLUMN marker DROP NOT NULL;
-ALTER TABLE external_repositories ALTER COLUMN release_date DROP NOT NULL;
