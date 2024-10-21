7a8a3912a1a38c7624e99806facc97f0688cf392
Edmore Moyo <etmoyo@gmail.com>
2024-10-21 13:21:06 -0400

    adds workflows controller and initial tests

---
+++ api/src/test/scala/com/pennsieve/api/TestWorkflowsController.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2021 University of Pennsylvania
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pennsieve.api
+
+import com.pennsieve.aws.cognito.MockCognito
+import com.pennsieve.helpers.{ DataSetTestMixin, MockAuditLogger }
+import org.json4s.jackson.Serialization.write
+
+class TestWorkflowsController extends BaseApiTest with DataSetTestMixin {
+
+  val auditLogger = new MockAuditLogger()
+  val mockCognito = new MockCognito()
+
+  override def afterStart(): Unit = {
+    super.afterStart()
+
+    addServlet(
+      new WorkflowsController(
+        insecureContainer,
+        secureContainerBuilder,
+        system,
+        auditLogger,
+        mockCognito,
+        system.dispatcher
+      ),
+      "/*"
+    )
+  }
+
+  test("create a workflow") {
+    val req = write(
+      CreateWorkflowRequest(
+        workflowName = "CyTOF-analysis-pipeline",
+        description = "Pipeline for running end-to-end analysis on CyTOF data",
+        secret = "secretkey"
+      )
+    )
+
+    postJson(s"/", req, headers = authorizationHeader(loggedInJwt)) {
+      status should equal(201)
+    }
+  }
+}
---
+++ core-models/src/main/scala/com/pennsieve/dtos/WorkflowDTO.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2021 University of Pennsylvania
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pennsieve.dtos
+import io.circe.generic.semiauto.{ deriveDecoder, deriveEncoder }
+import io.circe.{ Decoder, Encoder }
+
+final case class WorkflowDTO(tokenSecret: Option[APITokenSecretDTO])
+
+object WorkflowDTO {
+
+  implicit val encoder: Encoder[WorkflowDTO] = deriveEncoder[WorkflowDTO]
+  implicit val decoder: Decoder[WorkflowDTO] = deriveDecoder[WorkflowDTO]
+
+  def apply(tokenSecretDTO: Option[APITokenSecretDTO]): WorkflowDTO = {
+    tokenSecretDTO match {
+      case Some(tokenSecretDTO) =>
+        WorkflowDTO(Some(tokenSecretDTO))
+      case _ =>
+        WorkflowDTO(None)
+    }
+
+  }
+}
