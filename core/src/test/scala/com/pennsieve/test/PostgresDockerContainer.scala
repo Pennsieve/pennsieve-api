0b4d26bc6295624041b817d350d98f6779b3d0d7
John Frommeyer <johnfr@seas.upenn.edu>
2025-01-07 13:47:36 -0500

    Drop onboarding events endpoint (#323)
    * Remove code related to Onboarding events endpoints. Leave behind a stub that returns a 410 response code.
    
    * Drop the onboarding_events table.
    
    * Update to latest pennsievedb version for tests

--- core/src/test/scala/com/pennsieve/test/PostgresDockerContainer.scala
+++ core/src/test/scala/com/pennsieve/test/PostgresDockerContainer.scala
@@ -24,7 +24,7 @@ import java.time.Duration
 
 final class PostgresDockerContainerImpl
     extends PostgresContainerImpl(
-      dockerImage = "pennsieve/pennsievedb:V20241024144839"
+      dockerImage = "pennsieve/pennsievedb:V20241120161735"
     )
 
 trait PostgresDockerContainer extends StackedDockerContainer {
@@ -35,7 +35,7 @@ trait PostgresDockerContainer extends StackedDockerContainer {
 
 final class PostgresSeedDockerContainerImpl
     extends PostgresContainerImpl(
-      dockerImage = "pennsieve/pennsievedb:V20241024144839-seed"
+      dockerImage = "pennsieve/pennsievedb:V20241120161735-seed"
     )
 
 trait PostgresSeedDockerContainer extends StackedDockerContainer {
---
+++ migrations/src/main/resources/db/migrations/V20241120161735__drop_onboarding_events.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS onboarding_events;
