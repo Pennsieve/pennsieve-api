638e6e58bbbfc22e3b284b0381a7b8e638d3743e
Michael Uftring <michael.uftring@pennmedicine.upenn.edu>
2025-03-05 09:15:29 -0500

    Update S3ClientFactoryTest.scala
    fix duplicate test names

--- core/src/test/scala/com/pennsieve/aws/s3/S3ClientFactoryTest.scala
+++ core/src/test/scala/com/pennsieve/aws/s3/S3ClientFactoryTest.scala
@@ -5,14 +5,14 @@ import com.pennsieve.aws.s3.S3ClientFactory
 
 class S3ClientFactoryTest extends AnyFlatSpec with Matchers {
 
-  "S3ClientFactory" should "return the same client for the same region" in {
+  "S3ClientFactory" should "return the same client for the same region (use1)" in {
     val use1Client1 = S3ClientFactory.getClientForBucket("pennsieve-prod-storage-use1")
     val use1Client2 = S3ClientFactory.getClientForBucket("pennsieve-prod-logs-use1")
 
     use1Client1 shouldBe use1Client2
   }
 
-  "S3ClientFactory" should "return the same client for the same region" in {
+  "S3ClientFactory" should "return the same client for the same region (afs1)" in {
     val afs1Client1 = S3ClientFactory.getClientForBucket("pennsieve-prod-storage-afs1")
     val afsClient2 = S3ClientFactory.getClientForBucket("pennsieve-prod-logs-afs1")
 
