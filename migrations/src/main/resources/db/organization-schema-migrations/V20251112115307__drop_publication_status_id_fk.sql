a8d3c5417ce9d309eb4dea37ae167fb6d8d68497
Rohan Shah <rohanmalavshah@gmail.com>
2025-11-12 12:25:12 -0500

    drop trigger and function

--- migrations/src/main/resources/db/organization-schema-migrations/V20251112115307__drop_publication_status_id_fk.sql
+++ migrations/src/main/resources/db/organization-schema-migrations/V20251112115307__drop_publication_status_id_fk.sql
@@ -1,3 +1,9 @@
+-- Drop the trigger that automatically updates publication_status_id on datasets table
+DROP TRIGGER IF EXISTS dataset_publication_status_update_dataset ON dataset_publication_log;
+
+-- Drop the function that was used by the trigger
+DROP FUNCTION IF EXISTS add_dataset_publication_status();
+
 -- Drop the foreign key constraint from datasets table to dataset_publication_log
 ALTER TABLE datasets DROP CONSTRAINT IF EXISTS datasets_publication_status_id_fkey;
 
