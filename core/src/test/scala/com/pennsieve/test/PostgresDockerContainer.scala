7ea3f01e951f5d0360f4ee330283678a7f689672
John Frommeyer <johnfr@seas.upenn.edu>
2025-12-05 16:45:33 -0500

    Update version of pennsievedb DB used in tests. (#347)

--- core/src/test/scala/com/pennsieve/test/PostgresDockerContainer.scala
+++ core/src/test/scala/com/pennsieve/test/PostgresDockerContainer.scala
@@ -24,7 +24,7 @@ import java.time.Duration
 
 final class PostgresDockerContainerImpl
     extends PostgresContainerImpl(
-      dockerImage = "pennsieve/pennsievedb:V20250327131135"
+      dockerImage = "pennsieve/pennsievedb:V20251123185618"
     )
 
 trait PostgresDockerContainer extends StackedDockerContainer {
@@ -35,7 +35,7 @@ trait PostgresDockerContainer extends StackedDockerContainer {
 
 final class PostgresSeedDockerContainerImpl
     extends PostgresContainerImpl(
-      dockerImage = "pennsieve/pennsievedb:V20250327131135-seed"
+      dockerImage = "pennsieve/pennsievedb:V20251123185618-seed"
     )
 
 trait PostgresSeedDockerContainer extends StackedDockerContainer {
